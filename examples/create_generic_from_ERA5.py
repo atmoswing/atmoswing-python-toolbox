fb4fa99f9ea0ff78085364330b27625f01fc71ff
Pascal Horton <pascal.horton@giub.unibe.ch>
2021-02-16 21:05:40 +0100

    Creation of generic dataset from ERA5.

---
+++ examples/create_generic_from_ERA5.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import os
+from atmoswing.files.parse.predictors import netcdf_dataset
+from atmoswing.files.create.predictors import generic
+
+dir_origin = '...'
+dir_target = '...'
+
+files_list = [['pressure', 'cc', 'cc'],
+              ['pressure', 'd', 'd'],
+              ['pressure', 'pv', 'pv'],
+              ['pressure', 'r', 'r'],
+              ['pressure', 't', 't'],
+              ['pressure', 'u', 'u'],
+              ['pressure', 'v', 'v'],
+              ['pressure', 'w', 'w'],
+              ['pressure', 'z', 'z'],
+              ['single', 'cape', 'cape'],
+              ['single', 'cin', 'cin'],
+              ['single', 'deg0l', 'deg0l'],
+              ['single', 'lcc', 'lcc'],
+              ['single', 'msl', 'msl'],
+              ['single', 'slhf', 'slhf'],
+              ['single', 'sshf', 'sshf'],
+              ['single', 'ssr', 'ssr'],
+              ['single', 'ssrd', 'ssrd'],
+              ['single', 'str', 'str'],
+              ['single', 'strd', 'strd'],
+              ['single', 't2m', 't2m'],
+              ['single', 'tcc', 'tcc'],
+              ['single', 'tcw', 'tcw'],
+              ['single', 'tsr', 'tsr'],
+              ['single', 'ttr', 'ttr'],
+              ['single', 'u10', 'u10'],
+              ['single', 'v10', 'v10']]
+
+for file in files_list:
+    dir_origin_files = os.path.join(dir_origin, file[0])
+    dir_target_files = os.path.join(dir_target, file[0])
+    var_name_origin = file[1]
+    var_name_target = file[2]
+
+    reanalysis = netcdf_dataset.NetCDF(directory=dir_origin_files, file_pattern='*.nc', var_name=var_name_origin)
+    reanalysis.load()
+    new_reanalysis = generic.Generic(directory=dir_target_files, var_name=var_name_target, ref_data=reanalysis)
+    new_reanalysis.generate()
+
+
+
