5bf0bebf0ab123a901411fdbafa41fceea44cb07
Pascal Horton <pascal.horton@terranum.ch>
2023-05-12 11:02:35 +0200

    Revert "Fixing missing module attributes."
    This reverts commit d66dda94b4611ec50771efa53616746a4f527042.

--- atmoswing_toolbox/plots/__init__.py
+++ atmoswing_toolbox/plots/__init__.py
@@ -1,7 +1,5 @@
-from .. import has_eccodes, has_netcdf
 from .forecast_timeseries import ForecastTimeSeriesPlot
 from .gas_variables import GAsVariablesPlot
 from .montecarlo import MonteCarloPlot
 
-__all__ = ('has_netcdf', 'has_eccodes', 'ForecastTimeSeriesPlot', 'GAsVariablesPlot',
-           'MonteCarloPlot')
+__all__ = ('ForecastTimeSeriesPlot', 'GAsVariablesPlot', 'MonteCarloPlot')
