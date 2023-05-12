7f4463e2052ac3137b3f659c8b75d8e8eb6f594b
Pascal Horton <pascal.horton@terranum.ch>
2023-05-12 13:45:28 +0200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="atmoswing-toolbox",
-    version="1.3.6",
+    version="1.3.7",
     author="Pascal Horton",
     author_email="pascal.horton@giub.unibe",
     description="Python tools for AtmoSwing",
